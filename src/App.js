cd19d790cfada5fe48e408ae2f6ef7b840f5caaa
Kaan Kani İnce <kkince@newmind.ai>
2024-07-08 17:31:36 +0300

    custom modal popup created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ModalTest from "./components/custom-modal-popup/modal-test";
+
+function App() {
+    return (
+        <div className="App">
+            {/*Custom Modal Component*/}
+            <ModalTest/>
+        </div>
+    );
+}
+
+export default App;
